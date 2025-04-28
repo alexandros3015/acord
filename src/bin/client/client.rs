57b3ce78a9c6571703bf0269368a1d62c233cda2
alexandros3015 <alexandrosbeachtoys@gmail.com>
2025-04-27 22:34:07 -0400

    YAY IT WORKS

--- src/bin/client/client.rs
+++ src/bin/client/client.rs
@@ -1,57 +1,54 @@
-use tokio::io::{AsyncWriteExt, BufReader, AsyncBufReadExt};
-use tokio::net::TcpStream;
-use std::io::{Write, self};
-
-
-macro_rules! input {
-    () => {{
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        input.trim_end().to_string()
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+  };
+  use std::io::{self, Write};
+  
+  /// A tiny prompt‐and‐read helper
+  macro_rules! prompt {
+    ($fmt:expr $(, $arg:expr )* ) => {{
+      print!($fmt $(, $arg )*);
+      io::stdout().flush().unwrap();
+      let mut buf = String::new();
+      io::stdin().read_line(&mut buf).unwrap();
+      buf.trim_end().to_string()
     }};
-
-    ($($arg:tt)*) => {{
-        print!($($arg)*);
-        io::stdout().flush().unwrap();
-
-        input!()
-    }};
-}
-
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ip = input!("Enter the IP address of the server: ");
-    println!("Connecting to {}", ip);
-
-
-    let stream = TcpStream::connect(ip).await?;
+  }
+  
+  #[tokio::main]
+  async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // enter host:port
+    let addr = prompt!("server addr> ");
+    let nick = prompt!("name> ");
+    println!("connecting to {}", addr);
+    let stream = TcpStream::connect(addr).await?;
+  
+    // split into a read‐half and a write‐half
     let (read_half, mut write_half) = tokio::io::split(stream);
-
-    let mut reader = BufReader::new(read_half);
-    let mut line = String::new();
-
+  
+    // TASK A: read *any* server‐pushed lines and print them
+    let mut server_lines = BufReader::new(read_half).lines();
+    tokio::spawn(async move {
+      while let Ok(Some(line)) = server_lines.next_line().await {
+        println!("\r<< {}", line);
+        print!("you> ");
+        let _ = io::stdout().flush();
+      }
+      eprintln!("\n[-] server closed connection");
+    });
+  
+    // TASK B (main): prompt the user & send each line to the server
     loop {
-        let msg = input!("Enter a message: ");
-        write_half.write_all(msg.as_bytes()).await?;
-        write_half.write_all(b"\n").await?;
-        write_half.flush().await?;
+      let line = prompt!("> ");
+      let formatted_line = format!("{nick}: {line}");
 
-        line.clear();
-        let n = reader.read_line(&mut line).await?;
+      write_half.write_all(formatted_line.as_bytes()).await?;
+      write_half.write_all(b"\n").await?;
+      write_half.flush().await?;
 
-        if n == 0 {
-            println!("Connection closed");
-            break;
-        }
-
-        if msg == "!exit" {
-            println!("Exiting");
-            break;
-        }
-        
-        println!("Received: {}", line);
+      if line == "!exit" { break; }
     }
-
+  
     Ok(())
-}
+  }
+  
