aa9b262cf33058292f9b02266fc454707b981c91
alexandros3015 <105521378+alexandros3015@users.noreply.github.com>
2025-04-27 23:34:00 -0400

    small fix

--- src/bin/server/server.rs
+++ src/bin/server/server.rs
@@ -17,57 +17,50 @@ use tokio::{
 
   #[tokio::main]
   async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 1) bind the listener
+    
     let ip = prompt!("Enter host: ");
 
     let listener = TcpListener::bind(&ip).await?;
     println!("listening on {}", ip);
-  
-    // 2) create a broadcast channel for all client messages
+ 
     let (tx, _) = broadcast::channel::<String>(100);
   
     loop {
-      // 3) accept a new client
+      
       let (socket, peer) = listener.accept().await?;
       println!(">> {} connected", peer);
-  
-      // 4) clone the Sender for *this* client-handler,
-      //    and subscribe to receive everything
       let tx_for_send = tx.clone();
       let mut rx = tx.subscribe();
   
       let (read_half, mut write_half) = tokio::io::split(socket);
       let mut client_lines = BufReader::new(read_half).lines();
-  
-      // 5) spawn one task per client
+ 
       tokio::spawn(async move {
         loop {
           tokio::select! {
-            // a) client → server
+            
             result = client_lines.next_line() => match result {
               Ok(Some(line)) => {
-                // print server-side
-                println!("[{}] {}", peer, line.trim_end());
+                
+                println!("{}", line.trim_end());
   
-                // broadcast *this* client's line out to ALL subscribers
-                let _ = tx_for_send.send(format!("[{}] {}", peer, line.trim_end()));
+                let _ = tx_for_send.send(format!("{}", line.trim_end()));
               }
               _ => {
                 println!("<< {} disconnected", peer);
                 break;
               }
             },
-  
-            // b) server broadcast → this client
+ 
             result = rx.recv() => match result {
               Ok(msg) => {
-                // write the broadcasted msg + newline
+            
                 if write_half.write_all(msg.as_bytes()).await.is_err() { break; }
                 if write_half.write_all(b"\n").await.is_err()    { break; }
                 let _ = write_half.flush().await;
               }
               Err(_) => {
-                // channel closed or lagged
+            
                 break;
               }
             },
